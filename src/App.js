db21a5373c04683882a6f49318f647eedc19ba89
juanmana <juanmana0402@gmail.com>
2020-08-08 20:46:14 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+import Contacto from "./components/Contacto";
+import Nosotros from "./components/Nosotros";
+import Inicio from "./components/Inicio";
+import User from "./components/User";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container mt-5">
+        <div className='btn-group'>
+          <Link to ="/" className="btn btn-dark">
+           Inicio
+           </Link>
+           <Link to ="/Nosotros" className="btn btn-dark">
+           Nosotros
+           </Link>
+           <NavLink to ="/Contacto" className="btn btn-dark" activeClassName="active">
+           Contacto
+           </NavLink>
+        </div>
+        <hr />
+        <Switch>
+
+          <Route path="/nosotros/:id">
+
+                <User />
+
+          </Route>
+          <Route path="/contacto">
+            <Contacto />
+          </Route>
+
+          <Route path="/Nosotros">
+          <Nosotros />
+          </Route>
+
+
+          <Route path="/" exact>
+          <Inicio />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Contacto.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Contacto = () => {
+    return ( 
+
+
+        <h1>Contacto</h1>
+     );
+}
+ 
+export default Contacto;
